5556e5ea8f4b565a80e2d4492ce9d10efa3bf9da
SewonK <cutecat1128@naver.com>
2022-07-02 19:10:05 +0900

    [Feat] Create Custom Exception

---
+++ src/main/java/triple/backend/enums/StatusCode.java
@@ -0,0 +1,19 @@
+package triple.backend.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum StatusCode {
+    SUCCESS(200, "SUCCESS", "OK"),
+    NOT_FOUND(404,"COMMON-ERR-404","PAGE NOT FOUND"),
+    INTER_SERVER_ERROR(500,"COMMON-ERR-500","INTER SERVER ERROR"),
+    USER_NOT_FOUND(400,"USER-ERR-400","USER NOT FOUND"),
+    REVIEW_NOT_FOUND(400,"REVIEW-ERR-400","REVIEW NOT FOUND"),
+    ;
+
+    private int status;
+    private String code;
+    private String message;
+}
