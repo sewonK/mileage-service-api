d2f13db250c28ee7be742a3ea77998b5667aa4da
SewonK <cutecat1128@naver.com>
2022-07-03 19:57:28 +0900

    [Refactor] Add Repository annotation

--- src/main/java/triple/backend/repository/ReviewRepository.java
+++ src/main/java/triple/backend/repository/ReviewRepository.java
@@ -1,10 +1,12 @@
 package triple.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import triple.backend.entity.Review;
 
 import java.util.UUID;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
-
+    Review findFirstByPlaceIdOrderByCreatedDate(UUID placeId);
 }
