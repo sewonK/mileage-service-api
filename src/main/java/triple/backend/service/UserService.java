805b50b6847417a34ee5cbb9ec2f57b1b2c5f3b0
SewonK <cutecat1128@naver.com>
2022-07-03 19:52:04 +0900

    [Refactor] refactor user service & repository, change user controller to sample controller sample controller = make sample data(user and review)

--- src/main/java/triple/backend/service/UserService.java
+++ src/main/java/triple/backend/service/UserService.java
@@ -1,13 +1,13 @@
 package triple.backend.service;
 
-import triple.backend.dto.UserResponse;
+import triple.backend.dto.PointResponse;
 import triple.backend.entity.User;
 
 public interface UserService {
 
     User findById(String userId);
 
-    UserResponse saveUserById(String userId);
-
     int updatePoints(String userId, int diffPoint);
+
+    PointResponse saveUserById(String userId);
 }
--- src/main/java/triple/backend/service/UserServiceImpl.java
+++ src/main/java/triple/backend/service/UserServiceImpl.java
@@ -2,7 +2,7 @@ package triple.backend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import triple.backend.dto.UserResponse;
+import triple.backend.dto.PointResponse;
 import triple.backend.entity.User;
 import triple.backend.exception.UserNotFoundException;
 import triple.backend.repository.UserRepository;
@@ -20,17 +20,17 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public UserResponse saveUserById(String userId) {
-        User user = User.builder().userId(UUID.fromString(userId)).build();
-        userRepository.save(user);
-        return new UserResponse(userId, user.getPoint());
-    }
-
-    @Override
     public int updatePoints(String userId, int diffPoint) {
         User user = findById(userId);
         user.setPoint(user.getPoint() + diffPoint);
         userRepository.save(user);
         return user.getPoint();
+    }
+
+    @Override
+    public PointResponse saveUserById(String userId) {
+        User user = User.builder().userId(UUID.fromString(userId)).build();
+        userRepository.save(user);
+        return new PointResponse(userId, user.getPoint());
     }
 }
