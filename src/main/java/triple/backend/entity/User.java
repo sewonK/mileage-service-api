7c72d7da7616bdc7d4c9bb9b998da52436ea5842
SewonK <cutecat1128@naver.com>
2022-07-09 02:40:11 +0900

    [Refactor] Add lombok annotation in dtos

--- src/main/java/triple/backend/entity/User.java
+++ src/main/java/triple/backend/entity/User.java
@@ -10,7 +10,6 @@ import org.hibernate.annotations.Type;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.UUID;
 
 @Data
